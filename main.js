41032fee143227415b1d5ef5fb4c7cbb6c17f4ee
bennykoval <bethanykoval@gmail.com>
2019-07-09 22:18:54 -0400

    MOAR DESIGN

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 const arrayOfNums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 const allButtons = document.querySelectorAll('button') 
 const displayBox = document.querySelector('input')
+window.addEventListener('load', (event) => {
+    displayBox.focus();
+})
+
 for (let individualButton of allButtons) {
     individualButton.addEventListener('click', function () {
         if (individualButton.innerText in arrayOfNums) {
-            displayBox.value += individualButton.innerText
+            displayBox.value += individualButton.innerText;
+            displayBox.focus();
         }
 
         else if (individualButton.innerText === '=') {
-            displayBox.value = math.evaluate(displayBox.value)
+            displayBox.value = math.evaluate(displayBox.value);
+            displayBox.focus();
         }
 
         else if (individualButton.innerText === 'C') {
-            displayBox.value = ''
+            displayBox.value = '';
+            displayBox.focus();
         }
 
         else {
-            displayBox.value += individualButton.innerText
+            displayBox.value += individualButton.innerText;
+            displayBox.focus();
         }
     })
 }
